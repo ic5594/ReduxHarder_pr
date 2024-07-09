40faf178015bb217f0e4812d9eb9adb45a7f1684
ic5594 <ic5594@naver.com>
2024-07-09 16:56:03 +0900

    더미 상품 및 장바구니 넣기 기능 추가

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,26 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {id: 'p1', price: 6, title: 'MY First Book', descrpition: 'The First book I ever wrote'},
+  {id: 'p2', price: 5, title: 'MY Second Book', descrpition: 'The Second book I ever wrote'}
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            title={product.title}
+            key={product.id}
+            id={product.id}
+            price={product.price}
+            description={product.descrpition}
+          />
+        ))}
+        
       </ul>
     </section>
   );
