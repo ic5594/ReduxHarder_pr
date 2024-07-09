8f14c482b05543eaaca61c42088792221ca75b11
ic5594 <ic5594@naver.com>
2024-07-09 16:56:36 +0900

    장바구니 redux 동기화

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,17 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slilce';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch()
+
+  const addItems = () => {
+    dispatch(cartActions.addItemToCart({id, price, title}))
+  }
+  const removeItems = () => {
+    dispatch(cartActions.removeItemFromCart(id))
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +27,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItems}>-</button>
+          <button onClick={addItems}>+</button>
         </div>
       </div>
     </li>
